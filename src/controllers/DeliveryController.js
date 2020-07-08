133a52dbe704c0eadc68703a0bf2854085937cda
Nicolas <nicolasm15@hotmail.com>
2020-07-08 12:05:13 -0300

    feat: add mail sender in DeliveryController

--- src/controllers/DeliveryController.js
+++ src/controllers/DeliveryController.js
@@ -2,7 +2,7 @@ import Delivery from '../models/Delivery';
 import Deliveryman from '../models/Deliveryman';
 import Recipient from '../models/Recipient';
 import catchMessages from '../utils/catchMessages';
-// import Mail from '../lib/Mail';
+import Mail from '../lib/Mail';
 
 module.exports = {
 	async index(request, response) {
@@ -60,20 +60,30 @@ module.exports = {
 
 			await Delivery.create({ recipient_id, deliveryman_id, product });
 
-			// await Mail.sendMail({
-			// 	to: `${deliverymanExists.name} <${deliverymanExists.email}>`,
-			// 	subject: 'Nova entrega',
-			// 	text: 'Foi verificado que há uma nova entrega',
-			// });
+			await Mail.sendMail({
+				to: `${deliverymanExists.name} <${deliverymanExists.email}>`,
+				subject: 'Nova entrega',
+				template: 'newdelivery',
+				context: {
+					deliveryman: deliverymanExists.name,
+					product,
+					street: recipientExists.street,
+					number: recipientExists.number.toString(),
+					city: recipientExists.city,
+					state: recipientExists.state,
+					country: recipientExists.country,
+					zip_code: recipientExists.zip_code,
+				},
+			});
 
 			return response
 				.status(200)
 				.json({ recipient_id, deliveryman_id, product });
 		} catch (err) {
-			// if (!err.errors) {
-			// 	// Apenas desenvolvimento
-			// 	console.log(err);
-			// }
+			if (!err.errors) {
+				// Apenas desenvolvimento
+				console.log(err);
+			}
 			return response.status(400).json(catchMessages(err));
 		}
 	},
---
+++ src/lib/Mail.js
@@ -0,0 +1,47 @@
+// import nodemailerhbs from 'nodemailer-express-handlebars';
+// import 'express-handlebars';
+import nodemailer from 'nodemailer';
+import { resolve } from 'path';
+import handlebars from 'express-handlebars';
+import nodemailerhbs from 'nodemailer-express-handlebars';
+import mailConfig from '../config/mail';
+
+class Mail {
+	constructor() {
+		const { host, port, secure, auth } = mailConfig;
+		this.transporter = nodemailer.createTransport({
+			host,
+			port,
+			secure,
+			auth: auth.user ? auth : null,
+		});
+		this.configureTemplates();
+	}
+
+	configureTemplates() {
+		const viewsDir = resolve(__dirname, '..', 'views', 'emails');
+
+		this.transporter.use(
+			'compile',
+			nodemailerhbs({
+				viewEngine: handlebars.create({
+					layoutsDir: resolve(viewsDir, 'layouts'),
+					partialsDir: resolve(viewsDir, 'partials'),
+					defaultLayout: 'default',
+					extname: '.hbs',
+				}),
+				viewPath: viewsDir,
+				extName: '.hbs',
+			})
+		);
+	}
+
+	sendMail(message) {
+		return this.transporter.sendMail({
+			...mailConfig.default,
+			...message,
+		});
+	}
+}
+
+export default new Mail();
